f7fdb76df84b417c91b163a29557d5148153f7bf
Tobi Osimosu <osimosutobi@gmail.com>
2020-07-30 16:18:44 +0100

    Chore: Code refactoring

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,7 @@ const home = (req, res) => {
     pageName: 'Home',
   };
 
-  return renderPage(res, 'index', data, 'Demo Page');
+  return renderPage(res, 'pages/userDashboard', data, 'Demo Page');
 };
 
 module.exports = {
