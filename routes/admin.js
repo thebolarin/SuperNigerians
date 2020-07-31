d81b303ba6a55a6aaea63e87d8eaade3a745cdc0
youngGaf <youngabuhafiz@gmail.com>
2020-07-31 07:56:24 +0100

    added midddleware to guard admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
+const { authorizeAdmin } = require('../middleware/auth')
+
 const {
     dashboard, 
     deletePost,
 } = require('../controllers/admin');
 
 
-router.get('/dashboard', dashboard);
-router.delete('/delete/:postId', deletePost);
+router.get('/dashboard', authorizeAdmin, dashboard);
+router.delete('/delete/:postId', authorizeAdmin, deletePost);
 
 
 
