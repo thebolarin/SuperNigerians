04663c1d00a3f0fd7312aad7b091c41d5c9c81ef
Xemicolon <1vibenation@gmail.com>
2020-07-31 02:03:18 +0100

    Fix: Redirect if user not logged in and if user role is not user

--- controllers/userDashboard.js
+++ controllers/userDashboard.js
@@ -8,12 +8,24 @@ exports.getUserDetails = async (req, res) => {
     res.redirect("/login");
   }
   const user = await User.findOne({ _id: req.session.user._id });
+  if(user.role !== 'user'){
+      res.redirect('/')
+  }
   const userComments = await Comment.find({ creator: user._id });
   const userPosts = await Post.find({ creator: user._id });
+  const likes = []
+   userComments.forEach((comment) => {
+       likes.push(comment.like)
+  })
+  const numberOfLikes = likes.reduce((a, b) => {
+        return a + b
+  })
+  console.log(numberOfLikes)
   const data = {
     user,
     userComments,
     userPosts,
+    numberOfLikes
   };
   renderPage(res, "pages/userDashboard", data, "User Dashboard");
 };
