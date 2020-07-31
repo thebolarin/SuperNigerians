89ca6b26a641cf7e4fca3233753a06b88c787245
Xemicolon <1vibenation@gmail.com>
2020-07-31 01:41:49 +0100

    Fix: Redirect user if not logged in

--- controllers/userDashboard.js
+++ controllers/userDashboard.js
@@ -1,16 +1,19 @@
-const User = require('../models/user');
-const { renderPage } = require('../utils/render-page')
-const Comment = require('../models/comment');
-const Post = require('../models/post');
+const User = require("../models/user");
+const { renderPage } = require("../utils/render-page");
+const Comment = require("../models/comment");
+const Post = require("../models/post");
 
 exports.getUserDetails = async (req, res) => {
-    const user = await User.findOne({_id: req.session.user._id})
-    const userComments = await Comment.find({creator: req.session.user._id})
-    const userPosts = await Post.find({creator: req.session.user._id})
-    const data = {
-        user,
-        userComments,
-        userPosts
-    }
-    renderPage(res, 'pages/userDashboard', data, 'User Dashboard')
-}
+  if (!req.session.user) {
+    res.redirect("/");
+  }
+  const user = await User.findOne({ _id: req.session.user._id });
+  const userComments = await Comment.find({ creator: user._id });
+  const userPosts = await Post.find({ creator: user._id });
+  const data = {
+    user,
+    userComments,
+    userPosts,
+  };
+  renderPage(res, "pages/userDashboard", data, "User Dashboard");
+};
