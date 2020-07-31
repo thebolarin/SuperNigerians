96c514242a40294b7d8118e64f2a0355491768f9
youngGaf <youngabuhafiz@gmail.com>
2020-07-31 07:10:10 +0100

    refactored admin delete post feature

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const Post = require('../models/user');
 const { renderPage } = require('../utils/render-page');
 
 module.exports = {
@@ -7,42 +7,25 @@ module.exports = {
     renderPage(res, 'pages/adminDashboard', data, 'Admin | Dashboard', '/admin/dashboard');
   },
 
-  deletePost: async(req, res) => {
+  deletePost: async(req, res, next) => {
     try {
       const { postId } = req.params;
 
-      const userPost = await User.findone({ posts: postId });
+      const userPost = await Post.findById({ id: postId });
       if(!userPost){
           res.status(400).json({ status: 'error', message: 'Post does not exist'});
       }
 
-      const deletePost = await User.findOneAndDelete({ posts: postId })
+      const deletePost = await Post.findByIdAndRemove({ id: postId })
       if(!deletePost){
-          res.status(400).json({ status: 'error', message: 'An error occured while deleting post'});
+          res.flash('error', 'An error occured while deleting post');
       }
 
-      res.status(200).json({ status: 'success', message: 'Post deleted sucessfully'});
-    } catch (error) {
-        return res.status(500).send(error);
-    }
-  },
-
-  updatePost: async(req, res) => {
-    try {
-      const { postId } = req.params;
-      const userPost = await User.findone({ posts: postId });
-      if(!userPost){
-        res.status(400).json({ status: 'error', message: 'Post does not exist'});
-      }
-
-      const updatePost = await User.findOneAndUpdate({ posts: postId });
-      if(!updatePost){
-          res.status(400).json({ status: 'error', message: 'An error occured while updating post'});
-      }
-
-      res.status(200).json({ status: 'success', message: 'Post updated sucessfully'});
-    } catch (error) {
-      return res.status(500).send(error);
+      req.flash('Post deleted sucessfully');
+    } catch (err) {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     }
   },
 };
