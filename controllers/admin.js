eff60cffa24b66c94a59440c405b528a11b5815d
youngGaf <youngabuhafiz@gmail.com>
2020-08-01 02:56:35 +0100

    fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -63,33 +63,28 @@ module.exports = {
     renderPage(res, 'pages/adminDashboard', data, 'Admin | Dashboard', '/admin/dashboard');
   },
 
-  profile: async (req, res) => {
-    if (!req.session.user) {
-      res.redirect("/login");
-  }
-  const user = await User.findOne({ _id: req.session.user._id });
-  if (user.role !== "admin") {
-      res.redirect("/");
-  }
-  const userComments = await Comment.find({ creator: user._id });
-  const userPosts = await Post.find({ creator: user._id });
-  const likes = [];
-  let numberOfLikes = 0;
-  userComments.forEach((comment) => {
-      likes.push(comment.like);
-  });
-  if (likes.length !== 0) {
-      numberOfLikes = likes.reduce((a, b) => {
-          return a + b;
-      });
-  }
-  const data = {
-      user,
-      userComments,
-      userPosts,
-      numberOfLikes,
-  };
-      console.log(data)
+  profile: async (req, res, next) => {
+    try {
+    const { _id } = req.session.user
+    const user = await User.findOne({ _id });
+    const userComments = await Comment.find({ creator: _id });
+    const userPosts = await Post.find({ creator: _id });
+    const likes = [];
+    let numberOfLikes = 0;
+    userComments.forEach((comment) => {
+        likes.push(comment.like);
+    });
+    if (likes.length !== 0) {
+        numberOfLikes = likes.reduce((a, b) => {
+            return a + b;
+        });
+    }
+    const data = {
+        user,
+        userComments,
+        userPosts,
+        numberOfLikes,
+    };
     renderPage(
       res,
       "pages/adminProfile",
@@ -97,6 +92,11 @@ module.exports = {
       "Admin | Profile",
       "/admin/profile"
     );
+    } catch (err) {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    }
   },
 
   deletePost: async (req, res, next) => {
